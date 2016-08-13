8b5a357e54a95158e3ca4053be8ede47321a86e9
Kunal Kanojia <kunal@citadel.com.sg>
2016-08-14 00:22:50 +0800

    Actor based implementation of Word count using Akka

--- src/main/java/WordCountActor.java
+++ src/main/java/WordCountActor.java
@@ -1,13 +1,23 @@
+import akka.actor.ActorRef;
+import akka.actor.Props;
 import akka.actor.UntypedActor;
 
-import java.io.Serializable;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class WordCountActor {
 
-
     public static class FileToCount implements Serializable {
         public final String fileName;
+
         public FileToCount(String fileName) {
             this.fileName = fileName;
         }
@@ -16,6 +26,7 @@ public class WordCountActor {
     public static class WordCount implements Serializable {
         public final String fileName;
         public final int count;
+
         public WordCount(String fileName, int count) {
             this.fileName = fileName;
             this.count = count;
@@ -25,6 +36,7 @@ public class WordCountActor {
     public static class StartCounting implements Serializable {
         public final String docRoot;
         public final int numActors;
+
         public StartCounting(String docRoot, int numActors) {
             this.docRoot = docRoot;
             this.numActors = numActors;
@@ -33,6 +45,7 @@ public class WordCountActor {
 
     public static class WordCountSuccess implements Serializable {
         public final Map<String, Integer> result;
+
         public WordCountSuccess(Map<String, Integer> result) {
             this.result = result;
         }
@@ -40,10 +53,79 @@ public class WordCountActor {
 
 
     public static class WordCountMaster extends UntypedActor {
-        public void onReceive(Object message) {
-            if (message instanceof StartCounting){
+        List<String> files;
+        Map<String, Integer> wordCount = new HashMap<>();
+        ActorRef requester;
 
+
+        public void onReceive(Object message) {
+            if (message instanceof StartCounting) {
+                requester = sender();
+                StartCounting scm = (StartCounting) message;
+                List<ActorRef> workers = createWorkers(scm.numActors);
+                files = getFilesList(scm.docRoot);
+                beginSorting(files, workers);
+            } else if (message instanceof WordCount){
+                WordCount wc = (WordCount) message;
+                wordCount.put(wc.fileName, wc.count);
+                if(wordCount.size() == files.size()) {
+                    requester.tell(new WordCountSuccess(wordCount), self());
+                }
+            }
+        }
+
+        private List<ActorRef> createWorkers(int numActors) {
+            List<ActorRef> actorRefs = new ArrayList<>();
+            for (int i = 0; i < numActors; i++) {
+                actorRefs.add(context().actorOf(Props.create(WordCountWorker.class), "wc-worker-" + i));
+            }
+
+            return actorRefs;
+        }
+
+        private List<String> getFilesList(String rootPath) {
+            List<String> results = new ArrayList<>();
+            File[] files = new File(rootPath).listFiles();
+
+            for (File file : files != null ? files : new File[0]) {
+                if (file.isFile()) {
+                    results.add(file.getPath());
+                }
+            }
+            return results;
+        }
+
+        private void beginSorting(List<String> fileNames, List<ActorRef> workers) {
+            for (int i = 0; i < fileNames.size(); i++) {
+                workers.get(i % workers.size()).tell(new FileToCount(fileNames.get(i)), self());
             }
         }
     }
+
+    public static class WordCountWorker extends UntypedActor {
+
+        @Override
+        public void onReceive(Object message) throws Throwable {
+            if (message instanceof FileToCount) {
+                FileToCount ftc = (FileToCount) message;
+                int count = getWordCount(ftc.fileName);
+                sender().tell(new WordCount(ftc.fileName, count), self());
+            }
+
+        }
+
+        private Integer getWordCount(String fileName) {
+            Path filePath = Paths.get(fileName);
+            int count = 0 ;
+            try (BufferedReader reader = Files.newBufferedReader(filePath, UTF_8)) {
+                count = reader.lines().filter(l -> !l.trim().isEmpty()).mapToInt(l -> l.split(" ").length).sum();
+            } catch (IOException | UncheckedIOException e) {
+                System.out.println(e.getMessage());
+            }
+            return count;
+        }
+
+    }
 }
+
+
